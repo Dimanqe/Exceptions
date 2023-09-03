ab102bdca55af428543f07900c24aeb374398d7a
Dimanqe <d.sankeev@gmail.com>
2023-09-03 18:42:05 +0300

    Добавьте файлы проекта.

---
+++ Events/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceptions
+{
+    public class CustomException : Exception
+    {
+        public CustomException(string message) : base(message)
+        {
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Exception[] exceptions = new Exception[]
+            {
+            new IndexOutOfRangeException("Исключение типа IndexOutOfRangeException"),
+            new FileNotFoundException("Исключение типа FileNotFoundException"),
+            new DivideByZeroException("Исключение типа DivideByZeroException"),
+            new NullReferenceException("Исключение типа NullReferenceException"),
+            new CustomException("Собственное исключение")
+            };
+            try
+            {
+                foreach (var ex in exceptions)
+                {
+                    try
+                    {
+                        throw ex;
+                    }
+                    catch (Exception innerEx)
+                    {
+                        Console.WriteLine("Исключение: " + innerEx.Message);
+                    }
+                }
+            }
+            finally
+            {
+                Console.WriteLine("Блок finally выполнен.");
+            }
+        }
+    }
+}
+
